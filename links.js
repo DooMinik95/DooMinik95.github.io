05f30b56560f0bb03a48395fa2dc525af9072baf
Dominik Mees <dominik.mees@gmx.net>
2025-08-30 23:52:10 +0200

    [TASK] initial commit

---
+++ links.js
@@ -0,0 +1,9 @@
+const tileData = [
+    ["https://www.netflix.com","netflix.svg"],
+    ["https://www.amazon.de/video/","prime-video.svg"],
+    ["https://www.disneyplus.com/","disney-plus.svg"],
+    ["https://www3.xcine.io/","xcine.png"],
+    ["https://filmpalast.to/","filmpalast.png"],
+    ["https://primekiste.com/","primekiste.png"],
+    ["https://kinoger.to","kinoger.png"]
+];
---
+++ main.js
@@ -0,0 +1,23 @@
+function createTiles(tileData) {
+    const LINK=0, IMG=1
+    let tileCount = tileData.length
+    for (let i=0; i<tileCount; i++) {
+        let tileLink = tileData[i][LINK]
+        let tileImage = tileData[i][IMG]
+
+        const tileHtml = `
+            <div class="tile">
+                <a href="${tileLink}" target="_blank">
+                    <img src="./icons/${tileImage}" class="tile-image" height="200px" weight="200px" margin="5px"></img>
+                </a>
+            </div>
+        `
+
+        let tileHtmlNode = document.createElement("div")
+        tileHtmlNode.innerHTML = tileHtml
+
+        document.getElementById("tiles-container").append(tileHtmlNode)
+    }
+}
+
+createTiles(tileData)
