e6e9ffc9c910f5eb3f9093f05e808bcd0bd823f1
DooMinik95 <dominikmees18@gmail.com>
2026-08-02 11:15:46 +0200

    Update links.js
    added my yopad

--- links.js
+++ links.js
@@ -6,4 +6,5 @@ const tileData = [
     ["https://filmpalast.to/","filmpalast.png"],
     ["https://primekiste.com/","primekiste.png"],
     ["https://kinoger.to","kinoger.png"]
+    ["https://yopad.eu/p/dm-02082026-to-02082027-365days","yopad.png"]
 ];
